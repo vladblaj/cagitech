f507d761485539a54ccb533f3f527c48461c7019
Vlad Blaj <vlad.blaj@aeratechnology.com>
2021-07-07 13:13:47 +0300

    First Commit, deploy not set.

---
+++ src/pages/BlogDetails.js
@@ -0,0 +1,53 @@
+import { Helmet } from "react-helmet";
+import Disqus from "disqus-react";
+import React, { useEffect, useState } from "react";
+import MarkDown from "markdown-to-jsx";
+import Layout from "../components/Layout";
+
+function BlogDetails(props) {
+  const [content, setContent] = useState("");
+  const blogId = props.match.params.id;
+  const blogFile = props.match.params.title;
+
+  useEffect(() => {
+    import(`../blog/${blogFile}.md`)
+      .then((res) => {
+        fetch(res.default)
+          .then((res) => res.text())
+          .then((res) => setContent(res));
+      })
+      .catch((err) => console.log(err));
+  }, [blogFile]);
+
+  const disqusShortname = "chester-react"; //found in your Disqus.com dashboard
+  const disqusConfig = {
+    url: "https://tf-react-chester.now.sh/", //Homepage link of this site.
+    identifier: blogId,
+    title: blogFile,
+  };
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Blog Details - Chester React Personal Portfolio Template</title>
+        <meta
+          name="description"
+          content="Chester React Personal Portfolio Template Blog Details Page"
+        />
+      </Helmet>
+      <div className="mi-blog-details mi-section mi-padding-top mi-padding-bottom">
+        <div className="container">
+          <MarkDown>{content}</MarkDown>
+          <div className="mi-blog-details-comments mt-30">
+            <Disqus.DiscussionEmbed
+              shortname={disqusShortname}
+              config={disqusConfig}
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default BlogDetails;
