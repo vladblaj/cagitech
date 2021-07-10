596d7866492fef1417fc33824d3bd2c526c9c807
Vlad Blaj <marcoblaj@gmail.com>
2021-07-10 18:33:33 +0300

    Refactored to use async await.

--- functions/send-email.js
+++ functions/send-email.js
@@ -10,45 +10,34 @@ const headers = {
 
 const handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed', headers: { 'Allow': 'POST' } }
+    return {statusCode: 405, body: 'Method Not Allowed', headers: {'Allow': 'POST'}}
   }
   console.log('Send email started');
   const data = JSON.parse(event.body)
   if (!data.message || !data.name || !data.email || !data.subject) {
-    return { statusCode: 422, body: 'Name, email, message, subject and message are required.' }
+    return {statusCode: 422, body: 'Name, email, message, subject and message are required.'}
   }
   const {name, email, subject, message} = data
-  console.log('data', data);
-  console.log('client', client);
-
-  return client.transmissions.send({
-    options: {
-      sandbox: false,
-    },
-    content: {
-      from: {name: "Bitlads Software Site", email: "contact@bitladssoftware.com"},
-      subject,
-      html: generateTemplate(name, email, subject, message)
-    },
-    recipients: [{address: 'marcoblaj@gmail.com'},{address: 'bogdan.bitfoi@gmail.com'}],
-  })
-  .then(response => {
-    console.log('Mail has been sent successfully!')
-    console.log(response);
+  try {
+    const response = await client.transmissions.send({
+      options: {
+        sandbox: false,
+      },
+      content: {
+        from: {name: "Bitlads Software Site", email: "contact@bitladssoftware.com"},
+        subject,
+        html: generateTemplate(name, email, subject, message)
+      },
+      recipients: [{address: 'marcoblaj@gmail.com'}, {address: 'bogdan.bitfoi@gmail.com'}],
+    });
     callback(null, {
+      headers: {'Content-Type': 'application/json'},
       statusCode: successCode,
       body: JSON.stringify(response),
     })
-  })
-  .catch(err => {
-    console.log('Whoops! Something went wrong')
-    console.log(err)
-    callback(null, {
-      statusCode: 500,
-      body: error.toString()
-    })
-  })
-
+  } catch (error) {
+    callback(error);
+  }
 }
 const generateTemplate = (name, email, subject, message) => {
   return `<div>
